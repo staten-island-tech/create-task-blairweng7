843dda1253752b8799a07ae4076414f05caa121b
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2024-01-28 16:05:44 -0500

    jboiub

--- create-task/main.js
+++ create-task/main.js
@@ -1,15 +1,20 @@
+import './style.css'
+
 const DOMSelectors = {
-    quiz: document.getElementById("#quiz"),
-    button: document.getElementById("#button"),
+  quiz: document.getElementById("#quiz"),
+  button: document.getElementById("#button"),
 };
 
+
 const Questions = [
-    {
-        question: '',
-        answer: '',
-    },
-    {
-        question: '',
-        answer: '',
-    },
+  {
+      question: '',
+      answer: '',
+  },
+  {
+      question: '',
+      answer: '',
+  },
 ];
+
+
