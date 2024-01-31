551188c5f598004830a79f708956a6f27e67fd07
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2024-01-30 23:33:08 -0500

    salfnjeai

--- create-task/main.js
+++ create-task/main.js
@@ -7,13 +7,49 @@ const DOMSelectors = {
 
 const Questions = [
   {
-      question: '',
-      answer: '',
+      question: 'What is the capital of the United States?',
+      answer: 'Washington DC',
   },
   {
-      question: '',
-      answer: '',
+      question: 'What is the capital of Argentina?',
+      answer: 'Buenos Aires',
+  },
+  {
+    question: 'What is the capital of France?',
+    answer: 'Paris',
+  },
+  {
+    question: 'What is the capital of Netherlands?',
+    answer: 'Amsterdam',
+  },
+  {
+    question: 'What is the capital of Russia?',
+    answer: 'Moscow',
+  },
+  {
+    question: 'What is the capital of Switzerland?',
+    answer: 'Bern',
+  },
+  {
+    question: 'What is the capital of the United Kingdom?',
+    answer: 'London',
+  },
+  {
+    question: 'What is the capital of South Korea?',
+    answer: 'Seoul',
+  },
+  {
+    question: 'What is the capital of Norway?',
+    answer: 'Oslo',
+  },
+  {
+    question: 'What is the capital of Mexico?',
+    answer: 'Mexico City',
   },
 ];
 
-function
+function capital(){
+  for(let i=0; i < Questions.length; i++){
+    if (Questions(answer) === ${Questions.answer})
+  }
+}
