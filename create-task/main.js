bd7060a8143b9aa07e837482eebdcb5b5f7402ba
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2024-02-25 17:58:59 -0500

    jsaoifen

--- create-task/main.js
+++ create-task/main.js
@@ -1,6 +1,7 @@
 function calculateTip() {
   const billAmount = parseFloat(prompt("Enter the bill amount:"));
   
+  //or statement
   if (isNaN(billAmount) || billAmount <= 0) {
     console.log("Please enter a valid number for the bill amount.");
     return;
@@ -16,6 +17,7 @@ function calculateTip() {
   const tipAmount = (billAmount * tipPercentage) / 100;
   const totalAmount = billAmount + tipAmount;
 
+//to make the values 2 decimal places
   console.log(`Tip Amount: $${tipAmount.toFixed(2)}`);
   console.log(`Total Amount (including tip): $${totalAmount.toFixed(2)}`);
 }
